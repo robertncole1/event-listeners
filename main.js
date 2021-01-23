bc723567de3a89633c17394dbc6df90f546a21bf
Rob Cole <robertncole1@gmail.com>
2021-01-23 11:08:39 -0600

    working with callbacks

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+// console.log('Hello World');
+
+function saysHello(user) {
+  return `Hello ${user}!`; 
+}
+
+function saysGoodbye(user) {
+  return `Goodbye ${user}!`; 
+}
+
+function create(user, cb) {
+  return cb(user);
+}
+
+console.log(create('Rob', saysHello));
+console.log(create('Rob', saysGoodbye));
