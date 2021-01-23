be5d50e2dc0174331c906d62c3354441a06c758a
Rob Cole <robertncole1@gmail.com>
2021-01-23 10:57:08 -0600

    tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+// console.log('Hello World');
+
+// const button = document.getElementById('main-btn');
+
+// console.log(button);
+
+
+// const div = document.querySelector('#main-btn');
+
+// console.log(div);
