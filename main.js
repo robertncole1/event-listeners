960aa07cac433b1b5e740910c79e116a9ec916a8
Rob Cole <robertncole1@gmail.com>
2021-01-26 20:39:04 -0600

    array methods

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('Hello World');
+
+const array = [1,2,3,4,5,6]
+
+
+array.forEach((item) => {
+  console.log(item);
+})
+
+for (let i =0; i < array.length; i++) {
+  console.log(array[i]);
+}
